40cfc51252f795e74c2edbae32704f28ab7d9fac
Vincent van Hoef <vvanhoef@hotmail.com>
2020-12-11 16:17:31 +0100

    upddate

--- report/_build/jupyter_execute/analysis.r
+++ report/_build/jupyter_execute/analysis.r
@@ -1,5 +1,14 @@
-library("htmlwidgets")
-library(DT)
+library("kableExtra")
+library("knitr")
+library("openxlsx")
+library("ggplot2")
+library("PCAtools")
+library("factoextra")
+library("FactoMineR")
+library("cowplot")
+library("ggpubr")
+library("limma")
+library("openxlsx")
+db_file <- "../data/external/Human_GO_bp_no_GO_iea_symbol.gmt"
 
-
-This is text
+plot(1:250)
